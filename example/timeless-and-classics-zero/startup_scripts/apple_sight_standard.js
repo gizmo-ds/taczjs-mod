b16881c16011010f8756eb37f52e5748b2604ec7
Gizmo <me@aika.dev>
2024-11-09 09:14:21 +0800

    fix: correct inaccurate example

--- example/timeless-and-classics-zero/startup_scripts/apple_sight_standard.js
+++ example/timeless-and-classics-zero/startup_scripts/apple_sight_standard.js
@@ -1,7 +1,7 @@
 // 这是一个修改"机动军械师"中配方的例子, 这个例子中将"标准镜头"的配方修改为只需要9个苹果
 // "机动军械师"的下载地址: https://www.curseforge.com/minecraft/mc-mods/timeless-and-classics-zero
 
-TaCZJSEvents.recipeLoad((event) => {
+TaCZStartupEvents.recipeLoad((event) => {
     if (event.getId().toString() === "create_armorer:attachments/sight_standard") {
         const json = JSON.parse(event.getJson());
         json.materials = [
--- example/timeless-and-classics-zero/startup_scripts/attachment_remove.js
+++ example/timeless-and-classics-zero/startup_scripts/attachment_remove.js
@@ -1,7 +1,7 @@
 // 这是一个移除"机动军械师"中 EMX 配件的例子
 // "机动军械师"的下载地址: https://www.curseforge.com/minecraft/mc-mods/timeless-and-classics-zero
 
-TaCZJSEvents.attachmentIndexLoad((event) => {
+TaCZStartupEvents.attachmentIndexLoad((event) => {
     switch (event.getId().toString()) {
         case "create_armorer:scope_pipe": // 流体管道瞄准镜
         case "create_armorer:muzzle_pipe": // 流体管道枪口
